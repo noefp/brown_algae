a5991518e2b83e9a832e0a5ec376a7a3aa55ade9
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2025-10-07 14:35:46 +0200

    minor fixes

--- egdb_custom_text/welcome_text.php
+++ egdb_custom_text/welcome_text.php
@@ -2,9 +2,10 @@
   <br>
 
   <h1 style="font-size:32px;" class="text-center">Welcome to the <span style="color:#963;">Brown</span>Algae Atlas </h1>
+  <br>
 
-  <p style="font-size:18px;margin-top: 20px;" class="text-justify">
-    <strong><span style="color:#963;">Brown</span>Algae Atlas</strong> includes expression and genomics data fof brown algae. 
+  <p style="font-size:18px;margin-top: 20px;" class="text-center">
+    <strong><span style="color:#963;">Brown</span>Algae Atlas</strong> includes expression and genomics data for brown algae. 
   </p>
 
 
